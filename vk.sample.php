7ef70f9325b0f00dba61e35dd6984751f4b7c7c6
xbreaker <xbreaker@gmail.com>
2012-02-16 11:16:30 +0400

    Removed photo sample, because don't work.

--- vk.sample.php
+++ vk.sample.php
@@ -10,14 +10,11 @@
 if ( !class_exists( 'vk_wallpost' ) )
 		require('vk.wallpost.php' );
   
-$vk = new vk_wallpost("login", "pass", "http://vkontakte.ru/club9999", "-9999");
-/////////////////////////////////////////^ адрес стены (юзера или группы) ^ id стены группы с минусом, пользователя- без него
+$vk = new vk_wallpost("user_login", "user_pass", "http://vk.com/{wall_url}", "{target_wall_id}");
 //настройка прокси
-$vk->proxyAddr="server:port";
-$vk->proxyAuth="user:pass";
+$vk->proxyAddr="host:port";
+$vk->proxyAuth="login:pass";
 $vk->useProxy=false;
 //отправка сообщения
-$vk->postMessage("habr.ru", "Test message");
-//прикрепление изображения
-$vk->postPicture("habr.ru", "http://some.cool.image.jpg");
+$vk->postMessage("http://xbreaker.ru", "Test message");
 ?>
